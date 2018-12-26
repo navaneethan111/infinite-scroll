5029a22f2f73e658d755cc628a78fa6cf40f48bb
sathish r <sathishrajendr@gmail.com>
2018-12-26 16:19:53 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+ 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+ 
+import { HttpClientModule } from '@angular/common/http';
+ 
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +16,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,45 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { HttpClient } from '@angular/common/http';
+import "rxjs/add/operator/map";
+ 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  users=[];
+  page = 0;
+  maximumPages = 200;
+ 
+  constructor(public navCtrl: NavController, private httpClient: HttpClient) {
+    this.loadUsers();
+  }
+ 
+  loadUsers(infiniteScroll?) {
+ 
+    this.httpClient.get(`https://randomuser.me/api/?results=20&page=${this.page}`)
+    .subscribe(res => {
+      this.users = this.users.concat(res['results']);
+      if (infiniteScroll) {
+        infiniteScroll.complete();
+      }
+    })
+ 
+     
+ 
+     
+     
 
   }
-
+ 
+  loadMore(infiniteScroll) {
+    this.page++;
+    this.loadUsers(infiniteScroll);
+ 
+    if (this.page === this.maximumPages) {
+      infiniteScroll.enable(false);
+    }
+  }
+ 
 }
